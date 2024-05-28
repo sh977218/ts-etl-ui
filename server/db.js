09000091a9fe21f6ac261e0dca077810c682ce70
Peter Huang <peterhuang1113@gmail.com>
2024-05-28 15:25:44 -0400

    a

--- server/db.js
+++ server/db.js
@@ -3,6 +3,7 @@ import { Headers } from "node-fetch";
 
 export async function getCollection(collType) {
   const PANTRY_ID = process.env.PANTRY_ID;
+  let pr = '' || process.env.PR;
   let resp = await fetch(
     `https://getpantry.cloud/apiv1/pantry/${PANTRY_ID}/basket/${collType}`
   );
