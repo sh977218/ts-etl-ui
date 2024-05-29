88b882b7e0ca38c29617a302b76b8e5cabc44cd0
Peter Huang <peterhuang1113@gmail.com>
2024-05-29 10:20:56 -0400

    fix pr clean up script

--- clean-up-pantry-basket.js
+++ clean-up-pantry-basket.js
@@ -1,21 +1,17 @@
-import {pantryClient} from "./server/db.js";
+import fetch from "node-fetch";
 
 export async function deleteAllCollections() {
+  const PANTRY_ID = process.env.PANTRY_ID;
   let pr = '' || process.env.PR;
   return Promise.all(
     [`users`, `loadRequests`, `loadRequestActivities`, `versionQAs`]
       .map(collType => `${collType}${pr}`)
-      .map(collType => pantryClient.basket.delete('ToDoList'))
+      .map(collType => {
+        return fetch(
+          `https://getpantry.cloud/apiv1/pantry/${PANTRY_ID}/basket/${collType}`,
+          {
+            method: "DELETE"
+          })
+      })
   );
 }
-
-deleteAllCollections()
-  .then(() => {
-    console.log('All pantry baskets delete')
-  })
-  .catch((e) => {
-    console.log('Not all pantry baskets delete: ' + e)
-  })
-  .finally(() => {
-    console.log('finally block')
-  })
