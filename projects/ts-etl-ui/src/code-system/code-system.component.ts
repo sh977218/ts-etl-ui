ea1b8f884428460ac3a7053ce52d1a2ca6ef0b17
Peter Huang <peterhuang1113@gmail.com>
2024-06-05 15:43:26 -0400

    remove activityHistory row because it has its own table

--- projects/ts-etl-ui/src/code-system/code-system.component.ts
+++ projects/ts-etl-ui/src/code-system/code-system.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'app-code-system',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   templateUrl: './code-system.component.html',
 })
