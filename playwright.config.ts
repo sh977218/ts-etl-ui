385c31e5f12bec324f1dc3e2bc922e8772685d82
Peter Huang <peterhuang1113@gmail.com>
2024-06-06 09:18:42 -0400

    fix test

--- playwright.config.ts
+++ playwright.config.ts
@@ -80,7 +80,7 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'npm run start',
+        command: process.env.CI ? 'npm run serve' : 'npm run start',
         reuseExistingServer: true,
     },
 })
