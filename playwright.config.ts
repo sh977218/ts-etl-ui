5b4b8afecf0104cb2b1473319d5fb88923daf09a
Peter Huang <peterhuang1113@gmail.com>
2024-06-05 21:20:30 -0400

    enable playwright report to github page

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,9 +17,7 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    retries: 0,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -30,6 +28,7 @@ export default defineConfig({
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         video: 'on',
         trace: 'on',
+        headless: !!process.env.CI,
         screenshot: 'on',
         browserName: 'chromium',
         ignoreHTTPSErrors: true,
@@ -44,16 +43,16 @@ export default defineConfig({
         },
         {
             name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
+            use: {...devices['Desktop Chrome']},
         },
         {
             name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
+            use: {...devices['Desktop Firefox']},
         },
 
         {
             name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
+            use: {...devices['Desktop Safari']},
         },
 
         /* Test against mobile viewports. */
