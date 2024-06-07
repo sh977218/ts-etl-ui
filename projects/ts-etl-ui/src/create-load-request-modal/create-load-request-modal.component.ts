c6ae5c22ce0cce84e52ec8169b73e619002dd4eb
Peter Huang <peterhuang1113@gmail.com>
2024-06-07 14:58:07 -0400

    a

--- projects/ts-etl-ui/src/create-load-request-modal/create-load-request-modal.component.ts
+++ projects/ts-etl-ui/src/create-load-request-modal/create-load-request-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,53 +15,54 @@ import { UserService } from '../user-service';
 import { MatListModule } from "@angular/material/list";
 
 @Component({
-  standalone: true,
-  imports: [
-    FormsModule,
-    ReactiveFormsModule,
-    MatDialogModule,
-    MatButtonModule,
-    MatDialogModule,
-    ReactiveFormsModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatIconModule,
-      MatListModule
-  ],
-  templateUrl: './create-load-request-modal.component.html',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+    standalone: true,
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatDialogModule,
+        MatButtonModule,
+        MatDialogModule,
+        ReactiveFormsModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatIconModule,
+        MatListModule
+    ],
+    templateUrl: './create-load-request-modal.component.html',
+    styleUrls: ['./create-load-request-modal.component.scss'],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
 export class CreateLoadRequestModalComponent {
-  loadRequestCreationForm = new FormGroup(
-    {
-      codeSystemName: new FormControl<string>('', [Validators.required]),
-      type: new FormControl<string>('', [Validators.required]),
-      sourceFilePath: new FormControl<string>('', [Validators.required]),
-      requestSubject: new FormControl<string>('', [Validators.required]),
-      requester: new FormControl({value: '', disabled: true}),
-      requestTime: new FormControl({value: new Date(), disabled: true})
-    },
-  );
+    loadRequestCreationForm = new FormGroup(
+        {
+            codeSystemName: new FormControl<string>('', [Validators.required]),
+            type: new FormControl<string>('', [Validators.required]),
+            sourceFilePath: new FormControl<string>('', [Validators.required]),
+            requestSubject: new FormControl<string>('', [Validators.required]),
+            requester: new FormControl({value: '', disabled: true}),
+            requestTime: new FormControl({value: new Date(), disabled: true})
+        },
+    );
 
-  constructor(public http: HttpClient,
-              public dialogRef: MatDialogRef<CreateLoadRequestModalComponent>,
-              public userService: UserService) {
-    userService.user$.subscribe(user => this.loadRequestCreationForm.get('requester')?.setValue(user?.utsUser.username || ''))
-  }
+    constructor(public http: HttpClient,
+                public dialogRef: MatDialogRef<CreateLoadRequestModalComponent>,
+                public userService: UserService) {
+        userService.user$.subscribe(user => this.loadRequestCreationForm.get('requester')?.setValue(user?.utsUser.username || ''))
+    }
 
-  submitCreateReloadRequest() {
-    const newLoadRequest = this.loadRequestCreationForm.getRawValue();
-    this.http.post('/api/loadRequest', newLoadRequest)
-      .pipe(
-        tap({
-          next:()=>this.dialogRef.close('success'),
-          error:()=>this.dialogRef.close('error')
-        })
-      )
-      .subscribe()
-  }
+    submitCreateReloadRequest() {
+        const newLoadRequest = this.loadRequestCreationForm.getRawValue();
+        this.http.post('/api/loadRequest', newLoadRequest)
+            .pipe(
+                tap({
+                    next: () => this.dialogRef.close('success'),
+                    error: () => this.dialogRef.close('error')
+                })
+            )
+            .subscribe()
+    }
 
 }
