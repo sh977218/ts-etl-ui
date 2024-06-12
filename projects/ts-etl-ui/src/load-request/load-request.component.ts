3f533663439b9b1623759ad19da219e2acc99b7b
Peter Huang <peterhuang1113@gmail.com>
2024-06-12 09:10:30 -0400

    fix

--- projects/ts-etl-ui/src/load-request/load-request.component.ts
+++ projects/ts-etl-ui/src/load-request/load-request.component.ts
@@ -1,10 +1,10 @@
 import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  CUSTOM_ELEMENTS_SCHEMA,
-  NO_ERRORS_SCHEMA, signal,
-  ViewChild, WritableSignal
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA, signal,
+    ViewChild, WritableSignal
 } from '@angular/core';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
@@ -24,7 +24,7 @@ import { LoadRequestDataSource } from './load-request-data-source';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatSelectModule } from '@angular/material/select';
-import { catchError, filter, map, merge, of, startWith, Subject, switchMap, takeUntil } from 'rxjs';
+import { catchError, filter, map, merge, of, startWith, Subject, switchMap } from 'rxjs';
 
 import { LoadRequest, LoadRequestsApiResponse } from '../model/load-request';
 import { AlertService } from '../alert-service';
@@ -33,162 +33,155 @@ import { CreateLoadRequestModalComponent } from '../create-load-request-modal/cr
 import { LoadingService } from "../loading-service";
 import { triggerExpandTableAnimation } from "../animations";
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
-  selector: 'app-load-request',
-  standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    NgIf,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatTableModule,
-    MatButtonModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
-    MatDialogModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatCheckboxModule,
-    MatOptionModule,
-    MatSelectModule,
-    LoadRequestActivityComponent,
-    AsyncPipe
-  ],
-  templateUrl: './load-request.component.html',
-  animations: [triggerExpandTableAnimation],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+    selector: 'app-load-request',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        NgIf,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatTableModule,
+        MatButtonModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
+        MatSortModule,
+        MatPaginatorModule,
+        MatCheckboxModule,
+        MatOptionModule,
+        MatSelectModule,
+        LoadRequestActivityComponent,
+        AsyncPipe
+    ],
+    templateUrl: './load-request.component.html',
+    animations: [triggerExpandTableAnimation],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
 export class LoadRequestComponent implements AfterViewInit {
-  destroyed = new Subject<void>();
+    reloadAllRequests$ = new Subject();
+    displayedColumns: string[] = [
+        'requestId',
+        'type',
+        'codeSystemName',
+        'sourceFilePath',
+        'requestSubject',
+        'requestStatus',
+        'version',
+        'availableDate',
+        'requester',
+    ];
 
+    displayedColumnsForLargeScreen: string[] = ['requestTime']
 
-  reloadAllRequests$ = new Subject();
-  displayedColumns: string[] = [
-    'requestId',
-    'type',
-    'codeSystemName',
-    'sourceFilePath',
-    'requestSubject',
-    'requestStatus',
-    'version',
-    'availableDate',
-    'requester',
-  ];
+    columnsToDisplayWithExpand: WritableSignal<string[]> = signal([...this.displayedColumns, 'expand']);
 
-  displayedColumnsForLargeScreen: string[] = ['requestTime']
+    loadRequestDatabase: LoadRequestDataSource | null = null;
+    data: WritableSignal<LoadRequest[]> = signal([]);
 
-  columnsToDisplayWithExpand:WritableSignal<string[]> = signal([...this.displayedColumns, 'expand']);
+    expandedElement: LoadRequest | null = null;
 
-  loadRequestDatabase: LoadRequestDataSource | null = null;
-  data: WritableSignal<LoadRequest[]> = signal([]);
+    resultsLength = 0;
 
-  expandedElement: LoadRequest | null = null;
+    @ViewChild(MatPaginator, {static: false}) paginator!: MatPaginator;
+    @ViewChild(MatSort, {static: false}) sort!: MatSort;
 
-  resultsLength = 0;
+    searchCriteria = new FormGroup(
+        {
+            filters: new FormGroup({requestId: new FormControl()}),
+            requestDateType: new FormControl(0),
+            requestType: new FormControl(0),
+        }, {updateOn: 'submit',}
+    );
 
-  @ViewChild(MatPaginator, {static: false}) paginator!: MatPaginator;
-  @ViewChild(MatSort, {static: false}) sort!: MatSort;
+    constructor(private _httpClient: HttpClient,
+                public dialog: MatDialog,
+                private breakpointObserver: BreakpointObserver,
+                private loadingService: LoadingService,
+                public alertService: AlertService) {
+        breakpointObserver
+            .observe([
+                Breakpoints.Large,
+                Breakpoints.XLarge,
+            ])
+            .pipe(takeUntilDestroyed())
+            .subscribe(result => {
+                if (result.matches) {
+                    this.columnsToDisplayWithExpand.set([...this.displayedColumns, ...this.displayedColumnsForLargeScreen, 'expand']);
+                } else {
+                    this.columnsToDisplayWithExpand.set([...this.displayedColumns, 'expand']);
+                }
+            });
 
-  searchCriteria = new FormGroup(
-    {
-      filters: new FormGroup({requestId: new FormControl()}),
-      requestDateType: new FormControl(0),
-      requestType: new FormControl(0),
-    }, {updateOn: 'submit',}
-  );
+    }
 
-  constructor(private _httpClient: HttpClient,
-              public dialog: MatDialog,
-              private breakpointObserver: BreakpointObserver,
-              private loadingService: LoadingService,
-              public alertService: AlertService) {
-    breakpointObserver
-      .observe([
-        Breakpoints.Large,
-        Breakpoints.XLarge,
-      ])
-      .pipe(takeUntil(this.destroyed))
-      .subscribe(result => {
-        if (result.matches) {
-          this.columnsToDisplayWithExpand.set([...this.displayedColumns, ...this.displayedColumnsForLargeScreen, 'expand']);
-        } else {
-          this.columnsToDisplayWithExpand.set([...this.displayedColumns, 'expand']);
-        }
-      });
+    ngAfterViewInit() {
+        this.loadRequestDatabase = new LoadRequestDataSource(this._httpClient);
 
-  }
+        // If the user changes the sort order, reset back to the first page.
+        this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+        this.searchCriteria.valueChanges.subscribe(() => this.paginator.pageIndex = 0);
 
-  ngAfterViewInit() {
-    this.loadRequestDatabase = new LoadRequestDataSource(this._httpClient);
+        merge(this.reloadAllRequests$.pipe(filter(reload => !!reload)),
+            this.searchCriteria.valueChanges,
+            this.sort.sortChange,
+            this.paginator.page)
+            .pipe(
+                startWith({}),
+                switchMap(() => {
+                    this.loadingService.showLoading();
+                    const filters = this.searchCriteria.get('filters')?.getRawValue() || '';
+                    const sort = this.sort.active;
+                    const order = this.sort.direction;
+                    const pageNumber = this.paginator.pageIndex;
+                    const pageSize = this.paginator.pageSize
+                    return this.loadRequestDatabase!.getLoadRequests(
+                        filters, sort, order, pageNumber, pageSize
+                    ).pipe(catchError(() => of(null)));
+                }),
+                map((data: LoadRequestsApiResponse | null) => {
+                    if (data === null) {
+                        return [];
+                    }
 
-    // If the user changes the sort order, reset back to the first page.
-    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
-    this.searchCriteria.valueChanges.subscribe(() => this.paginator.pageIndex = 0);
+                    this.resultsLength = data.total_count;
+                    return data.items;
+                }),
+            )
+            .subscribe(data => {
+                this.data.set(data);
+                this.loadingService.hideLoading()
+            });
+    }
 
-    merge(this.reloadAllRequests$.pipe(filter(reload => !!reload)),
-      this.searchCriteria.valueChanges,
-      this.sort.sortChange,
-      this.paginator.page)
-      .pipe(
-        startWith({}),
-        switchMap(() => {
-          this.loadingService.showLoading();
-          const filters = this.searchCriteria.get('filters')?.getRawValue() || '';
-          const sort = this.sort.active;
-          const order = this.sort.direction;
-          const pageNumber = this.paginator.pageIndex;
-          const pageSize = this.paginator.pageSize
-          return this.loadRequestDatabase!.getLoadRequests(
-            filters, sort, order, pageNumber, pageSize
-          ).pipe(catchError(() => of(null)));
-        }),
-        map((data: LoadRequestsApiResponse | null) => {
-          if (data === null) {
-            return [];
-          }
+    openCreateLoadRequestModal() {
+        this.dialog.open(CreateLoadRequestModalComponent, {
+            width: '700px'
+        })
+            .afterClosed()
+            .subscribe({
+                next: res => {
+                    if (res === 'success') {
+                        this.alertService.addAlert('info', 'Successfully created load request.')
+                        this.reloadAllRequests$.next(true);
+                    } else if (res === 'error') {
+                        this.alertService.addAlert('danger', 'Error create load request.')
+                    }
+                }
+            });
+    }
 
-          this.resultsLength = data.total_count;
-          return data.items;
-        }),
-      )
-      .subscribe(data => {
-        this.data.set(data);
-        this.loadingService.hideLoading()
-      });
-  }
+    fetchLoadRequestActivity(event: MouseEvent, loadRequest: LoadRequest) {
+        this.expandedElement = this.expandedElement === loadRequest ? null : loadRequest
+        event.stopPropagation();
+    }
 
-  openCreateLoadRequestModal() {
-    this.dialog.open(CreateLoadRequestModalComponent, {
-      width: '700px'
-    })
-      .afterClosed()
-      .subscribe({
-        next: res => {
-          if (res === 'success') {
-            this.alertService.addAlert('info', 'Successfully created load request.')
-            this.reloadAllRequests$.next(true);
-          } else if (res === 'error') {
-            this.alertService.addAlert('danger', 'Error create load request.')
-          }
-        }
-      });
-  }
-
-  fetchLoadRequestActivity(event: MouseEvent, loadRequest: LoadRequest) {
-    this.expandedElement = this.expandedElement === loadRequest ? null : loadRequest
-    event.stopPropagation();
-  }
-
-  // @TODO get all pages
-  download() {
-    const blob = new Blob([JSON.stringify(this.data)], {type: 'application/json'});
-    saveAs(blob, 'loadRequests-export.json');
-    this.alertService.addAlert('', 'Export downloaded.');
-  }
-
-  ngOnDestroy() {
-    this.destroyed.next();
-    this.destroyed.complete();
-  }
+    // @TODO get all pages
+    download() {
+        const blob = new Blob([JSON.stringify(this.data)], {type: 'application/json'});
+        saveAs(blob, 'loadRequests-export.json');
+        this.alertService.addAlert('', 'Export downloaded.');
+    }
 }
