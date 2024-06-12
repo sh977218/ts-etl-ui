35ee4618aece865958f8f550b8407dadc6ea9a5b
Peter Huang <peterhuang1113@gmail.com>
2024-06-12 14:58:42 -0400

    format codes

--- projects/ts-etl-ui/src/load-request/load-request.component.ts
+++ projects/ts-etl-ui/src/load-request/load-request.component.ts
@@ -5,37 +5,37 @@ import {
   CUSTOM_ELEMENTS_SCHEMA,
   NO_ERRORS_SCHEMA, signal,
   ViewChild, WritableSignal,
-} from '@angular/core'
-import { AsyncPipe, NgIf } from '@angular/common'
-import { HttpClient } from '@angular/common/http'
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
-import { saveAs } from 'file-saver'
+} from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { saveAs } from 'file-saver';
 
-import { MatTableModule } from '@angular/material/table'
-import { MatInputModule } from '@angular/material/input'
-import { MatButtonModule } from '@angular/material/button'
-import { MatIconModule } from '@angular/material/icon'
-import { MatOptionModule } from '@angular/material/core'
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator'
-import { MatSort, MatSortModule } from '@angular/material/sort'
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatOptionModule } from '@angular/material/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-import { LoadRequestDataSource } from './load-request-data-source'
-import { MatCheckboxModule } from '@angular/material/checkbox'
-import { MatDialog, MatDialogModule } from '@angular/material/dialog'
-import { MatSelectModule } from '@angular/material/select'
-import { catchError, filter, map, merge, of, startWith, Subject, switchMap } from 'rxjs'
+import { LoadRequestDataSource } from './load-request-data-source';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { catchError, filter, map, merge, of, startWith, Subject, switchMap } from 'rxjs';
 
-import { LoadRequest, LoadRequestsApiResponse } from '../model/load-request'
-import { AlertService } from '../alert-service'
-import { LoadRequestActivityComponent } from '../load-request-activity/load-request-activity.component'
-import { CreateLoadRequestModalComponent } from '../create-load-request-modal/create-load-request-modal.component'
-import { LoadingService } from '../loading-service'
-import { triggerExpandTableAnimation } from '../animations'
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
-import { LoadRequestDetailComponent } from '../load-request-detail/load-request-detail.component'
-import { LoadRequestMessageComponent } from '../load-request-message/load-request-message.component'
+import { LoadRequest, LoadRequestsApiResponse } from '../model/load-request';
+import { AlertService } from '../alert-service';
+import { LoadRequestActivityComponent } from '../load-request-activity/load-request-activity.component';
+import { CreateLoadRequestModalComponent } from '../create-load-request-modal/create-load-request-modal.component';
+import { LoadingService } from '../loading-service';
+import { triggerExpandTableAnimation } from '../animations';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { LoadRequestDetailComponent } from '../load-request-detail/load-request-detail.component';
+import { LoadRequestMessageComponent } from '../load-request-message/load-request-message.component';
 
 @Component({
   selector: 'app-load-request',
@@ -65,7 +65,7 @@ import { LoadRequestMessageComponent } from '../load-request-message/load-reques
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class LoadRequestComponent implements AfterViewInit {
-  reloadAllRequests$ = new Subject()
+  reloadAllRequests$ = new Subject();
   displayedColumns: string[] = [
     'requestId',
     'codeSystemName',
@@ -75,20 +75,20 @@ export class LoadRequestComponent implements AfterViewInit {
     'requestTime',
     'requester',
     'creationTime',
-  ]
-  displayedColumnsForLargeScreen: string[] = []
+  ];
+  displayedColumnsForLargeScreen: string[] = [];
 
-  columnsToDisplayWithExpand: WritableSignal<string[]> = signal([...this.displayedColumns])
+  columnsToDisplayWithExpand: WritableSignal<string[]> = signal([...this.displayedColumns]);
 
-  loadRequestDatabase: LoadRequestDataSource | null = null
-  data: WritableSignal<LoadRequest[]> = signal([])
+  loadRequestDatabase: LoadRequestDataSource | null = null;
+  data: WritableSignal<LoadRequest[]> = signal([]);
 
-  expandedElement: LoadRequest | null = null
+  expandedElement: LoadRequest | null = null;
 
-  resultsLength = 0
+  resultsLength = 0;
 
-  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator
-  @ViewChild(MatSort, { static: false }) sort!: MatSort
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort!: MatSort;
 
   searchCriteria = new FormGroup(
     {
@@ -101,7 +101,7 @@ export class LoadRequestComponent implements AfterViewInit {
       requestDateType: new FormControl(0),
       requestType: new FormControl(0),
     }, { updateOn: 'submit' },
-  )
+  );
 
   constructor(private _httpClient: HttpClient,
               public dialog: MatDialog,
@@ -116,19 +116,19 @@ export class LoadRequestComponent implements AfterViewInit {
       .pipe(takeUntilDestroyed())
       .subscribe(result => {
         if (result.matches) {
-          this.columnsToDisplayWithExpand.set([...this.displayedColumns, ...this.displayedColumnsForLargeScreen])
+          this.columnsToDisplayWithExpand.set([...this.displayedColumns, ...this.displayedColumnsForLargeScreen]);
         } else {
-          this.columnsToDisplayWithExpand.set([...this.displayedColumns])
+          this.columnsToDisplayWithExpand.set([...this.displayedColumns]);
         }
-      })
+      });
   }
 
   ngAfterViewInit() {
-    this.loadRequestDatabase = new LoadRequestDataSource(this._httpClient)
+    this.loadRequestDatabase = new LoadRequestDataSource(this._httpClient);
 
     // If the user changes the sort order, reset back to the first page.
-    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0))
-    this.searchCriteria.valueChanges.subscribe(() => this.paginator.pageIndex = 0)
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+    this.searchCriteria.valueChanges.subscribe(() => this.paginator.pageIndex = 0);
 
     merge(this.reloadAllRequests$.pipe(filter(reload => !!reload)),
       this.searchCriteria.valueChanges,
@@ -137,28 +137,28 @@ export class LoadRequestComponent implements AfterViewInit {
       .pipe(
         startWith({}),
         switchMap(() => {
-          this.loadingService.showLoading()
-          const filters = this.searchCriteria.get('filters')?.getRawValue() || ''
-          const sort = this.sort.active
-          const order = this.sort.direction
-          const pageNumber = this.paginator.pageIndex
-          const pageSize = this.paginator.pageSize
+          this.loadingService.showLoading();
+          const filters = this.searchCriteria.get('filters')?.getRawValue() || '';
+          const sort = this.sort.active;
+          const order = this.sort.direction;
+          const pageNumber = this.paginator.pageIndex;
+          const pageSize = this.paginator.pageSize;
           return this.loadRequestDatabase!.getLoadRequests(
             filters, sort, order, pageNumber, pageSize,
-          ).pipe(catchError(() => of(null)))
+          ).pipe(catchError(() => of(null)));
         }),
         map((data: LoadRequestsApiResponse | null) => {
           if (data === null) {
-            return []
+            return [];
           }
-          this.resultsLength = data.total_count
-          return data.items
+          this.resultsLength = data.total_count;
+          return data.items;
         }),
       )
       .subscribe(data => {
-        this.data.set(data)
-        this.loadingService.hideLoading()
-      })
+        this.data.set(data);
+        this.loadingService.hideLoading();
+      });
   }
 
   openCreateLoadRequestModal() {
@@ -169,24 +169,24 @@ export class LoadRequestComponent implements AfterViewInit {
       .subscribe({
         next: res => {
           if (res === 'success') {
-            this.alertService.addAlert('info', 'Successfully created load request.')
-            this.reloadAllRequests$.next(true)
+            this.alertService.addAlert('info', 'Successfully created load request.');
+            this.reloadAllRequests$.next(true);
           } else if (res === 'error') {
-            this.alertService.addAlert('danger', 'Error create load request.')
+            this.alertService.addAlert('danger', 'Error create load request.');
           }
         },
-      })
+      });
   }
 
   fetchLoadRequestActivity(event: MouseEvent, loadRequest: LoadRequest) {
-    this.expandedElement = this.expandedElement === loadRequest ? null : loadRequest
-    event.stopPropagation()
+    this.expandedElement = this.expandedElement === loadRequest ? null : loadRequest;
+    event.stopPropagation();
   }
 
   // @TODO get all pages
   download() {
-    const blob = new Blob([JSON.stringify(this.data)], { type: 'application/json' })
-    saveAs(blob, 'loadRequests-export.json')
-    this.alertService.addAlert('', 'Export downloaded.')
+    const blob = new Blob([JSON.stringify(this.data)], { type: 'application/json' });
+    saveAs(blob, 'loadRequests-export.json');
+    this.alertService.addAlert('', 'Export downloaded.');
   }
 }
