5e770bf2ad5da83335e0d22c653befb9ef93bc98
Peter Huang <peterhuang1113@gmail.com>
2024-06-12 20:05:59 -0400

    reformat code

--- clean-up-mongo-db-for-pr.js
+++ clean-up-mongo-db-for-pr.js
@@ -1,15 +1,15 @@
-import { dropMongoCollection } from "./server/db.js";
+import { dropMongoCollection } from './server/db.js';
 
 const pr = process.env.PR || '';
 
 dropMongoCollection()
-    .then(() => {
-        console.log(`Dropped all mongo db of pr ${pr}`)
-    })
-    .catch((e) => {
-        console.log(`Not all mongo db of pr ${pr} deleted: ` + e)
-    })
-    .finally(() => {
-        console.log('finally block');
-        process.exit(0);
-    })
+  .then(() => {
+    console.log(`Dropped all mongo db of pr ${pr}`);
+  })
+  .catch((e) => {
+    console.log(`Not all mongo db of pr ${pr} deleted: ` + e);
+  })
+  .finally(() => {
+    console.log('finally block');
+    process.exit(0);
+  });
