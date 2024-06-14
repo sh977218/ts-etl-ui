151f207c282b23e1df5ff72ec297fd9a7eef9c23
Peter Huang <peterhuang1113@gmail.com>
2024-06-14 13:57:08 -0400

    using ci build

---
+++ projects/ts-etl-ui/src/environments/environment.ci.ts
@@ -0,0 +1,9 @@
+import { Environment } from '../model/environment';
+
+export const environment:Environment = {
+  /* eslint-disable */
+  environmentName: 'development',
+  ticketUrl:'/api/serviceValidate',
+  loginServiceUrl: '/login-cb?ticket=anything',
+  apiServer: ''
+};
