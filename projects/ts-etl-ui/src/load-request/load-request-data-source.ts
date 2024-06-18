f4a68c02f13b4103ac9124f2824936f5e959d551
ludetc <ludetc@gmail.com>
2024-06-18 08:56:55 -0400

    Search My Request

--- projects/ts-etl-ui/src/load-request/load-request-data-source.ts
+++ projects/ts-etl-ui/src/load-request/load-request-data-source.ts
@@ -10,6 +10,7 @@ export type LoadRequestFilter = {
   requestStatus: string;
   requestTime: string;
   requestDateRange: string;
+  requester: string
 }
 
 export class LoadRequestDataSource {
@@ -21,7 +22,7 @@ export class LoadRequestDataSource {
                   order: SortDirection = 'asc',
                   pageNumber: number = 0,
                   pageSize: number = 10) {
-    const {requestId, codeSystemName, requestSubject, type, requestStatus, requestTime, requestDateRange} = loadRequestFilter;
+    const {requestId, codeSystemName, requestSubject, type, requestStatus, requestTime, requestDateRange, requester} = loadRequestFilter;
     const params = {
       requestId,
       codeSystemName,
@@ -30,6 +31,7 @@ export class LoadRequestDataSource {
       requestStatus,
       requestTime,
       requestDateRange,
+      requester,
       sort,
       order,
       pageNumber,
