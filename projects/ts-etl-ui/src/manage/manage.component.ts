ce5dbdc479f46c5dc8a25946aff87b34b32192d5
Peter Huang <peterhuang1113@gmail.com>
2024-06-20 14:46:46 -0400

    add routing to all 3 tabs

--- projects/ts-etl-ui/src/manage/manage.component.ts
+++ projects/ts-etl-ui/src/manage/manage.component.ts
@@ -1,20 +1,19 @@
-import { AsyncPipe, NgIf } from '@angular/common';
+import { AsyncPipe, JsonPipe, NgForOf, NgIf } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { MatDrawer, MatDrawerContainer, MatSidenav, MatSidenavContainer } from '@angular/material/sidenav';
 import { MatListItem, MatNavList } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { MatMenuModule, } from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatTabsModule } from '@angular/material/tabs';
 
-import { UserService } from '../user-service';
 import { CodeSystemComponent } from '../code-system/code-system.component';
 import { LoadRequestComponent } from '../load-request/load-request.component';
 import { VersionQaComponent } from '../version-qa/version-qa.component';
+import { NavigationService } from '../navigation-service';
 
 @Component({
   selector: 'app-manage',
@@ -38,22 +37,14 @@ import { VersionQaComponent } from '../version-qa/version-qa.component';
     MatTabsModule,
     LoadRequestComponent,
     VersionQaComponent,
-    CodeSystemComponent
+    CodeSystemComponent,
+    NgForOf,
+    JsonPipe,
   ],
   templateUrl: './manage.component.html',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class ManageComponent {
-
-  constructor(public http: HttpClient,
-              public userService: UserService,
-              private router: Router,
-              public dialog: MatDialog) {
-    userService.user$.subscribe(u => {
-      if (!u) {
-        router.navigate(['please-log-in']);
-      }
-    });
+  constructor(public navigationService: NavigationService) {
   }
-
 }
