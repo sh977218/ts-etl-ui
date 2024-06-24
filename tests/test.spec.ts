532124dd072d9f1b2db69187b3e21df5c5c1fe3a
Peter Huang <peterhuang1113@gmail.com>
2024-06-24 15:54:16 -0400

    fix tests

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -78,10 +78,8 @@ test.describe('e2e test', async () => {
       await matDialog.getByLabel('Request Subject').fill('newly created load request');
       await matDialog.locator('[id="sourcePathFile"]').setInputFiles('./tests/glass.jpg');
       await expect(matDialog.getByLabel('Source File Path')).toContainText(`glass.jpg`);
-
       await expect(matDialog.getByRole('button', { name: 'Submit' })).toBeDisabled();
       await matDialog.getByLabel('Source File Path').fill(`file://nlmsombaserver.nlm.nih.gov/dev-ts-data-import/glass.jpg`);
-
       await matDialog.getByRole('button', { name: 'Submit' }).click();
       await matDialog.waitFor({ state: 'hidden' });
       await materialPo.checkAndCloseAlert(/Request \(ID: \d+\) created successfully/);
