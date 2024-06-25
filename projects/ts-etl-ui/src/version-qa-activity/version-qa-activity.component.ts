878df6d787e1f6a871d3c604c619f67af4eca66e
ludetc <ludetc@gmail.com>
2024-06-25 08:32:09 -0400

    Accpet / Reject buttons for Pending QA Status (#148)
    * Accpet / Reject buttons for Pending QA Status
    
    * fix compilation
    
    * fixed
    
    ---------
    
    Co-authored-by: Ludet <ludetc@nih.gov>
    Co-authored-by: Peter Huang <peterhuang1113@gmail.com>

--- projects/ts-etl-ui/src/version-qa-activity/version-qa-activity.component.ts
+++ projects/ts-etl-ui/src/version-qa-activity/version-qa-activity.component.ts
@@ -2,24 +2,17 @@ import {
   AfterViewInit,
   ChangeDetectionStrategy,
   Component,
-  Input,
-  OnInit,
+  computed,
+  input,
   ViewChild,
 } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialog } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { EMPTY, map, switchMap, tap } from 'rxjs';
 
-import {
-  VersionQaReviewDataReturn,
-  VersionQaReviewModalComponent,
-} from '../version-qa-review-modal/version-qa-review-modal.component';
 import { VersionQAActivity } from '../model/version-qa';
 import { VersionQaNoteComponent } from '../version-qa-note/version-qa-note.component';
 import { triggerExpandTableAnimation } from '../animations';
@@ -43,9 +36,9 @@ import { NgIf } from '@angular/common';
   animations: [triggerExpandTableAnimation],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VersionQaActivityComponent implements OnInit, AfterViewInit {
-  @Input() requestId: number | null = null;
-  @Input() versionQaActivities: VersionQAActivity[] = [];
+export class VersionQaActivityComponent implements AfterViewInit {
+  requestId = input.required<number>();
+  versionQaActivities = input.required<VersionQAActivity[]>();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -53,87 +46,22 @@ export class VersionQaActivityComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['id', 'activity', 'availableDate', 'reason', 'nbNotes'];
   expandedElement: VersionQAActivity | null = null;
 
-  dataSource: MatTableDataSource<VersionQAActivity> = new MatTableDataSource<VersionQAActivity>([]);
-
-  constructor(
-    public dialog: MatDialog,
-    private http: HttpClient,
-  ) {
-  }
-
-  ngOnInit() {
-    this.dataSource = new MatTableDataSource(this.versionQaActivities);
-  }
+  dataSource = computed(() => {
+    return new MatTableDataSource<VersionQAActivity>(this.versionQaActivities().reverse());
+  });
 
   ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.dataSource().paginator = this.paginator;
+    this.dataSource().sort = this.sort;
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.dataSource().filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+    if (this.dataSource().paginator) {
+      this.dataSource().paginator?.firstPage();
     }
-  }
-
-  action(action: 'Accept' | 'Reject') {
-    this.dialog
-      .open(VersionQaReviewModalComponent, {
-        width: '600px',
-        data: { tag: action },
-      })
-      .afterClosed()
-      .pipe(
-        // transform the data return from modal to `VersionQAActivityHistory`
-        map((versionQaReviewDataReturn: VersionQaReviewDataReturn | null) => {
-          if (versionQaReviewDataReturn) {
-            const { activity, id, availableDate, ...note } = versionQaReviewDataReturn;
-            return {
-              id,
-              availableDate,
-              activity,
-              updatedTime: new Date(),
-              notes: [note],
-            } as VersionQAActivity;
-          } else {
-            // if user click close button, 'versionQaReviewDataReturn' is null/undefined
-            return null;
-          }
-        }),
-        switchMap((versionQAActivity: VersionQAActivity | null) => {
-          if (versionQAActivity) {
-            return this.http.post('/api/qaActivity', {
-              requestId: this.requestId,
-              qaActivity: versionQAActivity,
-            })
-              .pipe(
-                /**
-                 * because this http post doesn't return any data,
-                 * but we need to pass the input from switchMap to next pipe,
-                 * so we can update the UI without fetch the entire array again
-                 */
-                map(() => versionQAActivity),
-              );
-          } else {
-            // if user click close button, we pass empty to next
-            return EMPTY;
-          }
-        }),
-        tap({
-          next: (versionQAActivityHistory) => {
-            if (versionQAActivityHistory) {
-              // this line is needed, so the Version QA table's row will reflect after collapse and expand again.
-              this.versionQaActivities.push(versionQAActivityHistory);
-              this.dataSource.data = this.versionQaActivities;
-            }
-          },
-        }),
-      )
-      // intentionally make `.subscribe() to be an empty, so using AsyncPipe (`| async`) in the HTML becomes very easy in future
-      .subscribe();
   }
 
 }
--- projects/ts-etl-ui/src/version-qa-review-modal/version-qa-review-modal.component.ts
+++ projects/ts-etl-ui/src/version-qa-review-modal/version-qa-review-modal.component.ts
@@ -1,73 +1,67 @@
-import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common'
-import { Component, Inject } from '@angular/core'
-import { MatButtonModule } from "@angular/material/button"
-import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from "@angular/material/dialog"
-import { MatCardModule } from "@angular/material/card";
-
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { UserService } from '../user-service';
+import { NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { provideNativeDateAdapter } from '@angular/material/core'
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
-export type VersionQaReviewDataReturn = {
-    id: string;
-    activity: string;
-    availableDate: Date;
-    createdTime: Date;
-    createdBy: string;
-    notes: string;
-    tag: string;
-}
+import { UserService } from '../user-service';
 
 @Component({
-    selector: 'app-version-qa-review-modal',
-    standalone: true,
-    providers: [provideNativeDateAdapter()],
-    imports: [
-        FormsModule,
-        NgSwitch,
-        NgSwitchCase,
-        NgSwitchDefault,
-        MatDialogModule,
-        MatButtonModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatCardModule,
-        MatDatepickerModule,
-        ReactiveFormsModule,
-    ],
-    templateUrl: './version-qa-review-modal.component.html'
+  selector: 'app-version-qa-review-modal',
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    FormsModule,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    NgForOf,
+  ],
+  templateUrl: './version-qa-review-modal.component.html',
 })
 export class VersionQaReviewModalComponent {
+  dataSource: { tag: string };
+  reviewForm = new FormGroup(
+    {
+      createdBy: new FormControl<string>({ value: '', disabled: true }),
+      activity: new FormControl<string>({ value: '', disabled: true }),
+      availableDate: new FormControl<Date>(new Date(), [Validators.required]),
+      notes: new FormArray([
+        new FormGroup({
+          notes: new FormControl<string>('', [Validators.required]),
+          createdTime: new FormControl<Date>(new Date()),
+          createdBy: new FormControl<string>({ value: '', disabled: true }),
+        }),
+      ]),
+    },
+  );
 
-    dataSource: { tag: string };
-    reviewForm = new FormGroup(
-        {
-            createdBy: new FormControl({value: '', disabled: true}),
-            tag: new FormControl({value: '', disabled: true}),
-            notes: new FormControl<string>('', [Validators.required]),
-            availableDate: new FormControl(new Date(), [Validators.required]),
-        },
-    );
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { tag: string },
+              public userService: UserService,
+              public dialog: MatDialog,
+  ) {
+    this.dataSource = data;
+    userService.user$.subscribe(user => {
+      this.reviewForm.get('createdBy')?.setValue(user?.utsUser.username || '');
+      this.notes.at(0)?.get('createdBy')?.setValue(user?.utsUser.username || '');
+    });
+    this.reviewForm.get('activity')?.setValue(this.dataSource.tag);
+  }
 
-    constructor(@Inject(MAT_DIALOG_DATA) public data: { tag: string },
-                public dialogRef: MatDialogRef<VersionQaReviewModalComponent>,
-                public userService: UserService,
-                public dialog: MatDialog,
-    ) {
-        this.dataSource = data;
-        userService.user$.subscribe(user => this.reviewForm.get('createdBy')?.setValue(user?.utsUser.username || ''))
-        this.reviewForm.get('tag')?.setValue(this.dataSource.tag);
-    }
-
-    save() {
-        this.dialogRef.close({
-            activity: this.dataSource.tag,
-            createdTime: new Date(),
-            ...this.reviewForm.getRawValue()
-        } as VersionQaReviewDataReturn);
-    }
+  get notes() {
+    return this.reviewForm.controls['notes'] as FormArray;
+  }
 
 }
