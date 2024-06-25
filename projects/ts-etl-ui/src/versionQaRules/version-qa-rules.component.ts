2293ff0f7afc8bd630a96345ca4f0053741f4b78
Ludet <ludetc@nih.gov>
2024-06-25 10:22:24 -0400

    Load Summary > QA Rules

---
+++ projects/ts-etl-ui/src/versionQaRules/version-qa-rules.component.ts
@@ -0,0 +1,57 @@
+import { AfterViewInit, Component, Input } from '@angular/core';
+import {
+  MatTableDataSource,
+  MatTableModule,
+} from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { LoadRule } from '../model/version-qa';
+
+@Component({
+  selector: 'app-version-qa-rules',
+  standalone: true,
+  imports: [
+    MatSortModule,
+    MatTableModule,
+  ],
+  templateUrl: './version-qa-rules.component.html',
+})
+export class VersionQaRulesComponent implements AfterViewInit {
+  @Input() qaRules!: LoadRule[];
+  rulesColumns = ['type', 'total', 'withData', 'error', 'warning', 'info'];
+  rulesData: {
+    type: string;
+    total: number;
+    withData: number;
+    errors: number;
+    warnings: number
+    infos: number;
+  }[] = [{
+    type: 'Verification',
+    total: 0,
+    withData: 0,
+    errors: 0,
+    warnings: 0,
+    infos: 0
+  },{
+    type: 'Validation',
+    total: 0,
+    withData: 0,
+    errors: 0,
+    warnings: 0,
+    infos: 0
+  }];
+  datasource = new MatTableDataSource(this.rulesData);
+
+  ngAfterViewInit(): void {
+    if (this.qaRules) {
+      this.qaRules.forEach(r => {
+        const index = r.type === 'Verification' ? 0 : 1;
+        this.rulesData[index].total++;
+        if (r.error) this.rulesData[index].errors++;
+        if (r.warning) this.rulesData[index].warnings++;
+        if (r.info) this.rulesData[index].infos++;
+      })
+    }
+  }
+
+}
