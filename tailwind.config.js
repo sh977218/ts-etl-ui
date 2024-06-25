c966ef363c17144476bfe15dc0f7aa2f359ee2f3
Peter Huang <peterhuang1113@gmail.com>
2024-06-25 09:54:40 -0400

    add isActive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./projects/ts-etl-ui/src/**/*.{html,ts}",
+    './projects/ts-etl-ui/src/**/*.{html,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'isActive': '#005cbb1a',
+      },
+    },
   },
   plugins: [],
-  important: true
-}
+  important: true,
+};
