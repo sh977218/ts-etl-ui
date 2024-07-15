a73f4c30f2140bc5462e01518ca67091040d91ca
Peter Huang <peterhuang1113@gmail.com>
2024-07-15 15:17:09 -0400

    qa report identification

--- projects/ts-etl-ui/src/load-version-report/load-version-report.component.ts
+++ projects/ts-etl-ui/src/load-version-report/load-version-report.component.ts
@@ -1,23 +1,76 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AsyncPipe, JsonPipe, KeyValuePipe, NgForOf, NgIf } from '@angular/common';
 import { LoadVersionDataSource } from '../load-version/load-version-data-source';
-import { catchError, map, of, switchMap, tap } from 'rxjs';
+import { map, shareReplay, switchMap, tap } from 'rxjs';
 import { ActivatedRoute, Params } from '@angular/router';
-import { LoadVersion } from '../model/load-version';
 import { HttpClient } from '@angular/common/http';
 import { LoadingService } from '../service/loading-service';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { LoadVersion } from '../model/load-version';
+import { CdkCopyToClipboard } from '@angular/cdk/clipboard';
 
 @Component({
   standalone: true,
   imports: [
     NgIf,
+    MatExpansionModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgForOf,
+    KeyValuePipe,
+    AsyncPipe,
+    CdkCopyToClipboard,
+    JsonPipe,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './load-version-report.component.html',
+  styleUrls: ['./load-version-report.component.scss'],
 })
-export class LoadVersionReportComponent implements AfterViewInit {
+export class LoadVersionReportComponent {
+  loadVersionKeys1: string[] = ['codeSystemName', 'version', 'loadNumber'];
+  loadVersionKeys2 = ['requestId', 'sourceFilePath'];
+  loadVersionDatabase: LoadVersionDataSource = new LoadVersionDataSource(this.http);
+  loadVersion$ = this.activatedRoute.paramMap
+    .pipe(
+      tap({ next: () => this.loadingService.showLoading() }),
+      map((params: Params) => {
+        return params['params']['requestId'];
+      }),
+      switchMap(requestId => {
+        return this.loadVersionDatabase.getLoadVersion(requestId)
+          .pipe(
+            tap({
+              next: () => this.loadingService.hideLoading(),
+              error: () => this.loadingService.hideLoading(),
+            }),
+          );
+      }),
+      shareReplay(1),
+    );
 
-  loadVersionDatabase: LoadVersionDataSource | null = null;
-  loadVersion: LoadVersion | null = null;
+  loadVersion1$ = this.loadVersion$
+    .pipe(
+      map((loadVersion: LoadVersion) => {
+        const filtered = Object.entries(loadVersion).filter(
+          ([k]) => this.loadVersionKeys1.includes(k),
+        );
+        return Object.fromEntries(filtered);
+      }),
+    );
+
+  loadVersion2$ = this.loadVersion$
+    .pipe(
+      map((loadVersion: LoadVersion) => {
+        const filtered = Object.entries(loadVersion).filter(
+          ([k]) => this.loadVersionKeys2.includes(k),
+        );
+        return Object.fromEntries(filtered);
+      }),
+    );
 
   constructor(private http: HttpClient,
               private activatedRoute: ActivatedRoute,
@@ -25,26 +78,7 @@ export class LoadVersionReportComponent implements AfterViewInit {
   ) {
   }
 
-  ngAfterViewInit(): void {
-    this.loadVersionDatabase = new LoadVersionDataSource(this.http);
-
-    this.activatedRoute.paramMap
-      .pipe(
-        tap({ next: () => this.loadingService.showLoading() }),
-        map((params: Params) => {
-          return params['params']['requestId'];
-        }),
-        switchMap(requestId => {
-          return this.loadVersionDatabase!.getLoadVersion(requestId).pipe(catchError(() => of(null)));
-        }),
-      )
-      .subscribe({
-        next: data => {
-          this.loadingService.hideLoading();
-          this.loadVersion = data;
-        },
-        error: () => this.loadingService.hideLoading(),
-      });
+  setStep() {
+    console.log('');
   }
-
 }
