ede435e77469ce2918a0179e227189632e8562ad
Peter Huang <peterhuang1113@gmail.com>
2024-07-30 22:16:15 -0400

    fix prod & integration ticketUrl

--- projects/ts-etl-ui/src/environments/environment.integration.ts
+++ projects/ts-etl-ui/src/environments/environment.integration.ts
@@ -3,7 +3,7 @@ import { Environment } from '../model/environment';
 export const environment: Environment = {
   /* eslint-disable */
   environmentName: 'integration',
-  ticketUrl: 'https://utslogin.nlm.nih.gov/serviceValidate', // instead of directly retrieving the user from UTS, it should pass ticket to TS backend and retrieve user from TS.
+  ticketUrl: '/portal-backend/api/serviceValidate',
   loginServiceUrl: 'https://login-prod.nlm.nih.gov/uts/login',
   apiServer: '/portal-backend/api',
 };
