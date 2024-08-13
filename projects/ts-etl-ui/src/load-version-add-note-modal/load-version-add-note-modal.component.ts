5fba87cf57477bd8fb98e2809ca26343f4b70b66
Ludet <ludetc@nih.gov>
2024-08-13 12:44:49 -0400

    hashtags as mat-chip

--- projects/ts-etl-ui/src/load-version-add-note-modal/load-version-add-note-modal.component.ts
+++ projects/ts-etl-ui/src/load-version-add-note-modal/load-version-add-note-modal.component.ts
@@ -1,12 +1,15 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { CommonModule, NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 
@@ -19,9 +22,11 @@ import { MatInputModule } from '@angular/material/input';
     NgSwitch,
     NgSwitchCase,
     NgSwitchDefault,
+    MatChipsModule,
     MatDialogModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatIcon,
     MatInputModule,
     MatCardModule,
     MatDatepickerModule,
@@ -40,26 +45,47 @@ export class LoadVersionAddNoteModalComponent {
     },
   );
 
-  get hashtags(): FormArray {
-    return this.addNoteForm.get('hashtags') as FormArray;
-  }
+  readonly hashtagsSig = signal<string[]>([]);
+  announcer = inject(LiveAnnouncer);
 
   constructor(public dialog: MatDialog,
   ) {
     // TODO hashtag is pre-filled with the ruleName, but I don't know what that is at the moment. So we will do later.
   }
 
-  sanitizeHashtags(i: number) {
-    const currentControl = this.hashtags.controls[i];
-    currentControl.setValue(currentControl.value.replace(/[^a-zA-Z0-9.\n]/g, ''));
-    this.hashtags.controls.forEach((fc, i) => {
-      if(this.hashtags.controls[i].value === '' && this.hashtags.controls.length !== (i+ 1)) {
-        this.hashtags.removeAt(i);
+  removeHashtag(hashtag: string) {
+    this.hashtagsSig.update(hashtagsSig => {
+      const index = hashtagsSig.indexOf(hashtag);
+      if (index < 0) {
+        return hashtagsSig;
       }
+
+      hashtagsSig.splice(index, 1);
+      this.announcer.announce(`removed ${hashtag}`);
+      return [...hashtagsSig];
     });
-    if(this.hashtags.controls[this.hashtags.controls.length - 1].value !== '') {
-      this.hashtags.push(new FormControl(''));
-    }
   }
+
+  addHashtag(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    if (value) {
+      this.hashtagsSig.update(hashtagsSig => [...hashtagsSig, value]);
+    }
+    event.chipInput!.clear();
+  }
+
+
+  // sanitizeHashtags(i: number) {
+  //   const currentControl = this.hashtags.controls[i];
+  //   currentControl.setValue(currentControl.value.replace(/[^a-zA-Z0-9.\n]/g, ''));
+  //   this.hashtags.controls.forEach((fc, i) => {
+  //     if(this.hashtags.controls[i].value === '' && this.hashtags.controls.length !== (i+ 1)) {
+  //       this.hashtags.removeAt(i);
+  //     }
+  //   });
+  //   if(this.hashtags.controls[this.hashtags.controls.length - 1].value !== '') {
+  //     this.hashtags.push(new FormControl(''));
+  //   }
+  // }
 
 }
