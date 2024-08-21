ae59ed70c74010eb5663955e5cf0fbb65f6d0d40
Ludet <ludetc@nih.gov>
2024-08-21 12:10:56 -0400

    Load REq Activities are not searchable

--- projects/ts-etl-ui/src/load-request-activity/load-request-activity.component.ts
+++ projects/ts-etl-ui/src/load-request-activity/load-request-activity.component.ts
@@ -58,13 +58,4 @@ export class LoadRequestActivityComponent implements OnInit, AfterViewInit {
     this.dataSource.sort = this.sort;
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
-
 }
