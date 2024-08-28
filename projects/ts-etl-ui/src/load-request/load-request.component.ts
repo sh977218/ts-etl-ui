27b19d716e72a803757b7edb250a1b8b5907497d
Peter Huang <peterhuang1113@gmail.com>
2024-08-28 09:54:12 -0400

    create time date range should be material range (#330)

--- projects/ts-etl-ui/src/load-request/load-request.component.ts
+++ projects/ts-etl-ui/src/load-request/load-request.component.ts
@@ -1,8 +1,14 @@
 import { AsyncPipe, CommonModule, DatePipe, NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import {
-  AfterViewInit, ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA,
-  signal, ViewChild,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  NO_ERRORS_SCHEMA,
+  signal,
+  ViewChild,
+  ViewEncapsulation,
   WritableSignal,
 } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
@@ -21,18 +27,9 @@ import { MatTableModule } from '@angular/material/table';
 import { ActivatedRoute, Params, Router, RouterModule } from '@angular/router';
 import { saveAs } from 'file-saver';
 import { assign } from 'lodash';
-import { default as _rollupMoment, Moment } from 'moment';
 import * as _moment from 'moment';
-import {
-  catchError,
-  filter,
-  map,
-  of,
-  startWith,
-  Subject,
-  switchMap,
-  tap,
-} from 'rxjs';
+import { default as _rollupMoment, Moment } from 'moment';
+import { catchError, filter, map, of, startWith, Subject, switchMap, tap } from 'rxjs';
 
 import { triggerExpandTableAnimation } from '../animations';
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
@@ -83,8 +80,10 @@ const moment = _rollupMoment || _moment;
     LoadRequestMessageComponent,
   ],
   templateUrl: './load-request.component.html',
+  styleUrls: ['./load-request.component.scss'],
   animations: [triggerExpandTableAnimation],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+  encapsulation: ViewEncapsulation.None,
   providers: [DatePipe],
 })
 export class LoadRequestComponent implements AfterViewInit {
@@ -124,8 +123,8 @@ export class LoadRequestComponent implements AfterViewInit {
       requestTimeFrom: new FormControl<Moment | string | null>(null),
       requestTimeTo: new FormControl<Moment | string | null>(null),
       requester: new FormControl<string | null>(null),
-      creationTimeFrom: new FormControl<string | null>(null),
-      creationTimeTo: new FormControl<string | null>(null),
+      creationTimeFrom: new FormControl<Moment | string | null>(null),
+      creationTimeTo: new FormControl<Moment | string | null>(null),
       filterRequestTime: new FormControl<string | null>(null, { updateOn: 'change' }),
       filterRequester: new FormControl<string | null>(null),
     }, { updateOn: 'submit' },
@@ -164,6 +163,14 @@ export class LoadRequestComponent implements AfterViewInit {
         const requestTimeTo = val.requestTimeTo;
         if (requestTimeTo) {
           queryParams.requestTimeTo = (requestTimeTo as Moment).toISOString();
+        }
+        const creationTimeFrom = val.creationTimeFrom;
+        if (creationTimeFrom) {
+          queryParams.creationTimeFrom = (creationTimeFrom as Moment).toISOString();
+        }
+        const creationTimeTo = val.creationTimeTo;
+        if (creationTimeTo) {
+          queryParams.creationTimeTo = (creationTimeTo as Moment).toISOString();
         }
         this.router.navigate(['load-requests'], {
           queryParamsHandling: 'merge',
@@ -237,6 +244,16 @@ export class LoadRequestComponent implements AfterViewInit {
             }
             if (searchCriteriaPatch.requester) {
               this.searchCriteria.controls.requester.patchValue(searchCriteriaPatch.requester, {
+                emitEvent: false,
+              });
+            }
+            if (searchCriteriaPatch.creationTimeFrom) {
+              this.searchCriteria.controls.creationTimeFrom.patchValue(moment(searchCriteriaPatch.creationTimeFrom), {
+                emitEvent: false,
+              });
+            }
+            if (searchCriteriaPatch.creationTimeTo) {
+              this.searchCriteria.controls.creationTimeTo.patchValue(moment(searchCriteriaPatch.creationTimeTo), {
                 emitEvent: false,
               });
             }
