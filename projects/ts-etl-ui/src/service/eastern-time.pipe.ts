2b52ec077e8e73ee99c9b1cbad33f0f8ab95aa21
Peter Huang <peterhuang1113@gmail.com>
2024-09-09 14:47:40 -0400

    show all date as yyyy mm dd instead of moment js default

--- projects/ts-etl-ui/src/service/eastern-time.pipe.ts
+++ projects/ts-etl-ui/src/service/eastern-time.pipe.ts
@@ -3,11 +3,11 @@ import moment from 'moment-timezone';
 
 @Pipe({
   name: 'easternTime',
-  standalone: true
+  standalone: true,
 })
 export class EasternTimePipe implements PipeTransform {
 
-  transform(value: Date | string, dateFormat: string = 'MMMM Do YYYY hh:mma z'): string {
+  transform(value: Date | string, dateFormat: string = 'YYYY-MM-DD z'): string {
     return moment(value).tz('America/New_York').format(dateFormat);
   }
 }
