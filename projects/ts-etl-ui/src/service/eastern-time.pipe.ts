ea1ec27c3d1b2da8f43b89eef5759cb1443a358d
Peter Huang <peterhuang1113@gmail.com>
2024-09-10 16:59:28 -0400

    fix empty date string

--- projects/ts-etl-ui/src/service/eastern-time.pipe.ts
+++ projects/ts-etl-ui/src/service/eastern-time.pipe.ts
@@ -8,6 +8,6 @@ import { format } from 'date-fns-tz';
 export class EasternTimePipe implements PipeTransform {
 
   transform(value: Date | string, dateFormat: string = 'yyyy-MM-dd hh:mm a z', timeZone = 'America/New_York'): string {
-    return format(value, dateFormat, { timeZone });
+    return value ? format(value, dateFormat, { timeZone }) : '';
   }
 }
