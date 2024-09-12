55b280d99094ee49378892ad3d02c2d8250c5641
ludetc <ludetc@gmail.com>
2024-09-12 09:14:55 -0400

    374 50 coverage (#378)
    * Add Coverage
    
    * 60% coverage
    
    * Ci runs on 3000, local on 4200
    
    * enforce coverage
    
    ---------
    
    Co-authored-by: Ludet <ludetc@nih.gov>

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -195,4 +195,64 @@ test.describe('e2e test', async () => {
     await page.getByRole('link', { name: 'Code System' }).click();
     await expect(page.getByRole('table').locator('tbody tr')).not.toHaveCount(0);
   });
+
+  test('Rule Message', async ({ page }) => {
+    await page.goto('/load-version-report/0');
+    const firstRow = 'app-load-version-report-rule-message tbody tr:first-of-type';
+    await expect(page.locator(firstRow)).toContainText('Code.Hierarchy.OrphanCode');
+    await expect(page.locator(firstRow)).toContainText('Error');
+    await expect(page.locator(firstRow)).toContainText('Code D90012 is not mapped to a term');
+
+    await page.locator('#messageGroupSearch').selectOption('Warning');
+    await expect(page.locator(firstRow)).not.toContainText('Code D90012 is not mapped to a term');
+  });
+
+  test('QA Rules', async ({ page }) => {
+    await page.goto('/load-version-report/0');
+    const row = 'app-load-version-report-rule tbody tr:nth-of-type(2)';
+    await expect(page.locator(row)).toContainText('Code.DuplicateCode');
+    await expect(page.locator(row)).toContainText('No duplicate codes in cs_code table');
+    await expect(page.locator(`${row} td:nth-of-type(4)`)).toContainText('5');
+
+    await page.locator('app-load-version-report-rule #nameFilterInput').fill('QaCount');
+    await page.keyboard.press('Enter');
+
+    await expect(page.locator(row)).not.toContainText('No duplicate codes in cs_code table');
+    await expect(page.locator(row)).toContainText('Raw counts are not present');
+  });
+
+  test('Note Filters', async ({ page }) => {
+    await page.goto('/load-versions');
+    await page.locator('tbody tr td .fake-link', {hasText: '20231012080001'}).click();
+
+    const tbody = 'app-load-version-note tbody';
+
+    await expect(page.locator(tbody)).toContainText('TestTag');
+    await expect(page.locator(tbody)).toContainText('Tag.3');
+
+    await page.locator('#hashtagsFilterInput').selectOption('Tag2');
+    await expect(page.locator(tbody)).toContainText('Note2');
+    await expect(page.locator(tbody)).not.toContainText('TestTag');
+    await expect(page.locator(tbody)).not.toContainText('Tag.3');
+
+    await page.locator('#hashtagsFilterInput').selectOption('ALL');
+    await expect(page.locator(tbody)).toContainText('TestTag');
+    await expect(page.locator(tbody)).toContainText('Tag.3');
+
+    await page.locator('#noteInput').fill('Second');
+    await expect(page.locator(tbody)).toContainText('Note2');
+    await expect(page.locator(tbody)).not.toContainText('TestTag');
+    await expect(page.locator(tbody)).not.toContainText('Tag.3');
+
+    await page.locator('#noteInput').clear();
+    await page.locator('#createdBySearchInput').selectOption('ludetc');
+    await expect(page.locator(tbody)).toContainText('Note2');
+    await expect(page.locator(tbody)).not.toContainText('TestTag');
+    await expect(page.locator(tbody)).not.toContainText('Tag.3');
+
+    await page.locator('#createdBySearchInput').selectOption('ALL');
+    await expect(page.locator(tbody)).toContainText('TestTag');
+    await expect(page.locator(tbody)).toContainText('Tag.3');
+
+  });
 });
