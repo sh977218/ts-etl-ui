4f27e6dcb434da622ee4b8920ae8c6e846644552
Peter Huang <peterhuang1113@gmail.com>
2024-09-16 22:05:30 -0400

    nyc code coverage not working

--- tests/code-system.spec.ts
+++ tests/code-system.spec.ts
@@ -11,8 +11,17 @@ test.describe('CS - ', async () => {
     await page.goto('/code-systems');
     await expect(page.locator('#codeSystemsListTable > tbody')).toContainText('LOINC');
 
-    await page.locator('#codeSystemSearchInput').fill('ICD');
+    await page.locator('#codeSystemSearchInput').fill('ICD10CM');
     await page.keyboard.press('Enter');
     await expect(page.locator('#codeSystemsListTable > tbody')).not.toContainText('LOINC');
+    await page.locator('table tbody tr:first-of-type').first().click();
+    await page.getByPlaceholder('Ex. Draft').fill('HPO2021_10_10');
+    await page.keyboard.press('Enter');
+    await expect(page.getByText('Rejected')).not.toHaveCount(0);
+
+    await page.getByText('HPO2021_10_10').first().click();
+    await page.getByPlaceholder('Ex. PT').first().fill('1007412');
+    await page.keyboard.press('Enter');
+    await expect(page.getByText('ambroxol / doxycycline')).not.toHaveCount(0);
   });
 });
