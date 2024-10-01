68689cefd1d906a5eaaab3921d40f9c3b10dc747
Ludet <ludetc@nih.gov>
2024-10-01 07:00:31 -0400

    request frst / last name in LR create

--- projects/ts-etl-ui/src/create-load-request-modal/create-load-request-modal.component.ts
+++ projects/ts-etl-ui/src/create-load-request-modal/create-load-request-modal.component.ts
@@ -65,7 +65,7 @@ export class CreateLoadRequestModalComponent {
         [Validators.required, sourceFilePathValidator()]),
       requestSubject: new FormControl<string>('', [Validators.required]),
       notificationEmail: new FormControl(this.userService.user?.email, [Validators.required, Validators.email]),
-      requester: new FormControl<string>({ value: this.userService.user!.utsUser.username!, disabled: true }),
+      requester: new FormControl<string>({ value: `${this.userService.user!.utsUser.firstName} ${this.userService.user!.utsUser.lastName}`, disabled: true }),
       requestTime: new FormControl<Date>({ value: new Date(), disabled: true }),
     },
   );
