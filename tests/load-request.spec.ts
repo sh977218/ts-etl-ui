7fa468654a5da1a88f25ad576572fb1fcea4ac3c
Peter Huang <peterhuang1113@gmail.com>
2024-10-14 11:30:12 -0400

    fix tests

--- tests/load-request.spec.ts
+++ tests/load-request.spec.ts
@@ -47,7 +47,7 @@ test.describe('LR - ', async () => {
     await expect(page.locator('body')).toContainText('This application requires you to log in');
   });
 
-  test('Load Request table', async ({ page, materialPo }) => {
+  test('Load Request table @smoke', async ({ page, materialPo }) => {
     const matDialog = materialPo.matDialog();
 
     await expect(page.getByRole('link', { name: 'Load Request' })).toBeVisible();
@@ -118,7 +118,6 @@ test.describe('LR - ', async () => {
       await page.getByPlaceholder('Req. ID').fill(newlyCreatedLoadRequestId);
       await page.getByPlaceholder('Any Request date').click();
       await materialPo.matOption().filter({ hasText: `Today's` }).click();
-      await page.getByRole('button', { name: 'Search' }).click();
       await materialPo.waitForSpinner();
 
       await expect(page.locator('td:has-text("Scheduled")')).toBeVisible();
@@ -161,7 +160,6 @@ test.describe('LR - ', async () => {
       // next 2 lines might fall, if the test runs first step on Saturday 11:59 PM and this step runs on Sunday 00:00 AM. This week's filter will fail. But this is very unlikely
       await page.getByPlaceholder('Any Request date').click();
       await materialPo.matOption().filter({ hasText: `This week's` }).click();
-      await page.getByRole('button', { name: 'Search' }).click();
       await materialPo.waitForSpinner();
 
       await expect(page.locator('td:has-text("Emergency")')).toBeVisible();
@@ -194,8 +192,8 @@ test.describe('LR - ', async () => {
     await test.step(`search for newly 'Cancelled'/'Emergency' load request`, async () => {
       await page.getByRole('link', { name: 'Load Request' }).click();
       await materialPo.selectMultiOptions(page.locator(`[id="requestStatusFilterSelect"]`), ['Cancelled']);
+      await materialPo.waitForSpinner();
       await materialPo.selectMultiOptions(page.locator(`[id="requestTypeFilterSelect"]`), ['Emergency']);
-      await page.getByRole('button', { name: 'Search' }).click();
       await materialPo.waitForSpinner();
       await expect(page.locator('td:has-text("Emergency")')).toBeVisible();
       await expect(page.locator('td:has-text("CPT")')).toBeVisible();
@@ -276,7 +274,6 @@ test.describe('LR - ', async () => {
   test('URL Status Filter', async ({ page, materialPo }) => {
     await page.goto('/load-requests');
     await materialPo.selectMultiOptions(page.locator(`[id="requestStatusFilterSelect"]`), ['Stopped']);
-    await page.getByRole('button', { name: 'Search' }).click();
     await expect(page.locator(firstCell)).toHaveText('30');
   });
 
