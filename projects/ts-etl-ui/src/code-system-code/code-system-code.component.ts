149491be49110869b736e2e2a7373febdf5ed645
Peter Huang <peterhuang1113@gmail.com>
2024-10-17 22:08:50 -0400

    a

--- projects/ts-etl-ui/src/code-system-code/code-system-code.component.ts
+++ projects/ts-etl-ui/src/code-system-code/code-system-code.component.ts
@@ -1,51 +1,49 @@
-import { NgForOf, NgIf } from "@angular/common";
+import { NgForOf, NgIf } from '@angular/common';
 import { AfterViewInit, ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
-import { MatButtonModule } from "@angular/material/button";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
-import { MatListModule } from "@angular/material/list";
-import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
-import { MatSort, MatSortModule } from "@angular/material/sort";
-import { MatTableDataSource, MatTableModule } from "@angular/material/table";
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
-import { triggerExpandTableAnimation } from "../animations";
-import { CodeSystemCode } from "../model/code-system";
+import { triggerExpandTableAnimation } from '../animations';
+import { CodeSystemCode } from '../model/code-system';
 
 @Component({
-    selector: 'app-code-system-code',
-    standalone: true,
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    animations: [triggerExpandTableAnimation],
-    imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatButtonModule, MatIconModule, NgIf, MatListModule, NgForOf],
-    templateUrl: './code-system-code.component.html',
+  selector: 'app-code-system-code',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [triggerExpandTableAnimation],
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatButtonModule, MatIconModule, NgIf, MatListModule, NgForOf],
+  templateUrl: './code-system-code.component.html',
 })
 export class CodeSystemCodeComponent implements OnInit, AfterViewInit {
 
-    @ViewChild(MatPaginator, {static: false}) paginator!: MatPaginator;
-    @ViewChild(MatSort, {static: false}) sort!: MatSort;
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort!: MatSort;
 
-    @Input() codes: CodeSystemCode[] = []
+  @Input() codes: CodeSystemCode[] = [];
 
-    columnsToDisplay: string[] = ["code", "name", "status", "termType", "isActive"]
+  columnsToDisplay: string[] = ['code', 'name', 'status', 'termType', 'isActive'];
 
-    dataSource: MatTableDataSource<CodeSystemCode> = new MatTableDataSource<CodeSystemCode>([]);
+  dataSource: MatTableDataSource<CodeSystemCode> = new MatTableDataSource<CodeSystemCode>([]);
 
-    ngOnInit() {
-        this.dataSource = new MatTableDataSource(this.codes);
-    }
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource(this.codes);
+  }
 
-    ngAfterViewInit() {
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-    }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
-    applyFilter(event: Event) {
-        const filterValue = (event.target as HTMLInputElement).value;
-        this.dataSource.filter = filterValue.trim().toLowerCase();
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
-        if (this.dataSource.paginator) {
-            this.dataSource.paginator.firstPage();
-        }
-    }
+    this.dataSource.paginator!.firstPage();
+  }
 }
--- projects/ts-etl-ui/src/service/user-service.ts
+++ projects/ts-etl-ui/src/service/user-service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { isEmpty } from 'lodash';
 import { CookieService } from 'ngx-cookie-service';
-import { BehaviorSubject, finalize, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, switchMap, tap } from 'rxjs';
 
 import { AlertService } from './alert-service';
 
@@ -74,13 +74,11 @@ export class UserService {
   }
 
   logOut() {
+    this._user$.next(null);
+    this.cookieService.delete('Bearer');
     this.http.post(`${environment.logoutUrl}`, {})
       .pipe(
         switchMap(() => this.router.navigate(['/please-log-in'])),
-        finalize(() => {
-          this._user$.next(null);
-          this.cookieService.delete('Bearer');
-        }),
       )
       .subscribe();
   }
