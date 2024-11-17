dd0f25b4b4e4fee750cddabaf4c31bfe847631b1
Peter Huang <peterhuang1113@gmail.com>
2024-11-16 21:56:29 -0500

    a

---
+++ server/create-mongo-db.js
@@ -0,0 +1,15 @@
+import { createMongoCollections } from './db.js';
+
+createMongoCollections()
+  .then(() => {
+    console.log('Create DB successfully from create-mongo-db.js');
+    process.exit(0);
+  })
+  .catch((reason) => {
+    console.log('Create DB failed from create-mongo-db.js' + ` ${reason}`);
+    process.exit(1);
+  })
+  .finally(() => {
+    console.log('Create DB final callback from create-mongo-db.js');
+    process.exit(0);
+  });
