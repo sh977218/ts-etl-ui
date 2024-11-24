4c4c93e1ee89643f6e0c304491deca190d8fdbd8
Peter Huang <peterhuang1113@gmail.com>
2024-11-24 17:55:23 -0500

    code refactory (#485)

--- projects/ts-etl-ui/src/load-request-detail/load-request-detail.component.ts
+++ projects/ts-etl-ui/src/load-request-detail/load-request-detail.component.ts
@@ -10,7 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ActivatedRoute, Params, RouterLink } from '@angular/router';
-import { filter, map, shareReplay, switchMap } from 'rxjs';
+import { filter, map, switchMap, tap } from 'rxjs';
 
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 import {
@@ -113,7 +113,6 @@ export class LoadRequestDetailComponent {
   @ViewChild('loadRequestMessageList') sort!: MatSort;
 
   displayedColumns: string[] = ['key', 'value'];
-  messageColumns = ['type', 'tag', 'message', 'time'];
 
   user: User | null | undefined = undefined;
 
@@ -126,51 +125,6 @@ export class LoadRequestDetailComponent {
               private userService: UserService,
   ) {
     userService.user$.subscribe(user => this.user = user);
-    this.loadRequest$.pipe(
-      map(lr => {
-
-        const loadRequestSummary = lr.loadRequestSummary;
-        return [...Object.keys(loadRequestSummary), 'loadElapsedTime', 'numberOfMessages']
-          .filter(k => !['_id', 'requesterUsername', 'version', 'loadRequestActivities', 'loadRequestMessages', 'availableDate', 'loadComponents'].includes(k))
-          .sort((a, b) => LABEL_SORT_ARRAY.indexOf(a) - LABEL_SORT_ARRAY.indexOf(b))
-          .reduce<string[]>((acc, key) => {
-            acc.push(key);
-            if (['creationTime'].includes(key)) {
-              acc.push('spacer');
-            }
-            return acc;
-          }, [])
-          .map(key => {
-            const result: RowElement = {
-              /* istanbul ignore next */
-              label: LABEL_MAPPING[key] || `something wrong about ${key}`,
-              key,
-              value: loadRequestSummary[key as keyof LoadRequestSummary],
-            };
-
-            if (key === 'spacer') {
-              return {
-                label: '',
-                key: 'spacer',
-                value: '',
-              };
-            }
-
-            if (key === 'numberOfMessages') {
-              result.value = lr.loadRequestSummary.loadRequestMessageList.length || 0;
-            }
-
-            if (key === 'loadElapsedTime') {
-              result.value = new Date(lr.loadRequestSummary.loadEndTime).getTime() - new Date(lr.loadRequestSummary.loadStartTime).getTime();
-            }
-
-            return result;
-          });
-      })).subscribe((data) => {
-      this.dataSource = new MatTableDataSource(data);
-      this.dataSource.sort = this.sort;
-      this.dataSource.sortingDataAccessor = easternTimeMaSortingDataAccessor;
-    });
   }
 
   loadRequest$ = this.activatedRoute.paramMap
@@ -182,7 +136,51 @@ export class LoadRequestDetailComponent {
         return this.http.get<LoadRequestDetailResponse>(`${environment.apiServer}/load-request/${requestId}`)
           .pipe(map(res => res.result.data));
       }),
-      shareReplay(1),
+      tap({
+        next: (lr) => {
+
+          const loadRequestSummary = lr.loadRequestSummary;
+          const data = [...Object.keys(loadRequestSummary), 'loadElapsedTime', 'numberOfMessages']
+            .filter(k => !['_id', 'requesterUsername', 'version', 'loadRequestActivities', 'loadRequestMessages', 'availableDate', 'loadComponents'].includes(k))
+            .sort((a, b) => LABEL_SORT_ARRAY.indexOf(a) - LABEL_SORT_ARRAY.indexOf(b))
+            .reduce<string[]>((acc, key) => {
+              acc.push(key);
+              if (['creationTime'].includes(key)) {
+                acc.push('spacer');
+              }
+              return acc;
+            }, [])
+            .map(key => {
+              const result: RowElement = {
+                /* istanbul ignore next */
+                label: LABEL_MAPPING[key] || `something wrong about ${key}`,
+                key,
+                value: loadRequestSummary[key as keyof LoadRequestSummary],
+              };
+
+              if (key === 'spacer') {
+                return {
+                  label: '',
+                  key: 'spacer',
+                  value: '',
+                };
+              }
+
+              if (key === 'numberOfMessages') {
+                result.value = lr.loadRequestSummary.loadRequestMessageList.length || 0;
+              }
+
+              if (key === 'loadElapsedTime') {
+                result.value = new Date(lr.loadRequestSummary.loadEndTime).getTime() - new Date(lr.loadRequestSummary.loadStartTime).getTime();
+              }
+
+              return result;
+            });
+          this.dataSource = new MatTableDataSource(data);
+          this.dataSource.sort = this.sort;
+          this.dataSource.sortingDataAccessor = easternTimeMaSortingDataAccessor;
+        },
+      }),
     );
 
   isTime(key: string) {
